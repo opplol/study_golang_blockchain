5f9bd473a6b58855117ef698c18c024e4467d45a
opplol <colra03@gmail.com>
2022-02-11 18:46:40 +0900

    code refactor for blockchain

--- app/main.go
+++ app/main.go
@@ -1,49 +1,19 @@
 package main
 
 import (
-	"crypto/sha256"
 	"fmt"
+	"go_crypo_coin/blockchain"
 )
 
-type block struct {
-	data     string
-	hash     string
-	prevHash string
-}
-
-type blockchain struct {
-	blocks []block
-}
-
-func (c *blockchain) getLastHash() string {
-
-	if len(c.blocks) > 0 {
-		return c.blocks[len(c.blocks)-1].hash
-	}
-	return ""
-}
-
-func (c *blockchain) addBlock(data string) {
-	newBlock := block{data, "", c.getLastHash()}
-	hash := sha256.Sum256([]byte(newBlock.data + newBlock.prevHash))
-	newBlock.hash = fmt.Sprintf("%x", hash)
-	c.blocks = append(c.blocks, newBlock)
-}
-
-func (c *blockchain) listBlocks() {
-	for _, block := range c.blocks {
-		fmt.Printf("Data: %s\n", block.data)
-		fmt.Printf("Hash: %s\n", block.hash)
-		fmt.Printf("PrevHash: %s\n", block.prevHash)
-	}
-}
-
 func main() {
-	chain := blockchain{}
+  chain := blockchain.GetBlockchain()
+	chain.AddBlock("Second block")
+	chain.AddBlock("Third block")
+	chain.AddBlock("Four block")
 
-	chain.addBlock("Genesis Block")
-	chain.addBlock("Second Block")
-	chain.addBlock("Third Block")
-	chain.listBlocks()
-
+	for _, block := range chain.AllBlocks() {
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %s\n", block.Hash)
+		fmt.Printf("Prev Hash: %s\n", block.PrevHash)
+	}
 }
